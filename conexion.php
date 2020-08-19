222172e892f617b28d7290c074e64628f577b461
ferney1438123 <ferney1438123@gmail.com>
2020-08-18 23:42:26 -0500

    consulta db

--- conexion.php
+++ conexion.php
@@ -4,4 +4,17 @@
   $email = $_POST['email'];
   $contra = md5($_POST['pass']);
   $mysql->conectar();
+
+  $Consultar = $mysql->efectuarConsulta("select");
+            
+ 
+  if(!empty($Consultar)){
+      
+      if(mysqli_num_rows($Consultar) > 0){
+         
+          while ($resultado = mysqli_fetch_assoc($Consultar)){
+             
+            header("location: login.php");
+            die();
+          }
   ?>
