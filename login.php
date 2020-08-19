c56f566d1b6fa28030bfa279d9a1229e8ec51f47
ferney1438123 <ferney1438123@gmail.com>
2020-08-19 17:27:33 -0500

    falta warning

--- login.php
+++ login.php
@@ -25,7 +25,7 @@
     <input type="password" class="form-control" id="pass" name="pass">
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <input type="checkbox" class="form-check-input" id="check" name="check" required>
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
   <button type="submit" class="btn btn-primary btn-block">Submit</button>
