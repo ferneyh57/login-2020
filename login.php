89e7ed57dd2a4ffc1ded375ed5a0cc63ed9a71e1
ferney1438123 <ferney1438123@gmail.com>
2020-08-18 22:40:19 -0500

    carpeta bosstrap

--- login.php
+++ login.php
@@ -4,11 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    link
     <title>Document</title>
 </head>
 <body>
     <form method="POST" action=""></form>
     <label for="">Usuario</label>
+    <input type="user">
+    <div></div>
     <label for="">Contraseña</label>
+    <input type="password">
+  <input type="submit">
 </body>
 </html>
